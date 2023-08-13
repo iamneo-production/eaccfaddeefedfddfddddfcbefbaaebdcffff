66980565c10b98731c5a4bc77d6754683698ffc3
Iamneo <techadmin@iamneo.ai>
2023-08-13 10:14:26 +0000

    Backup Commit

--- testng.java
+++ testng.java
@@ -1,45 +1,44 @@
-import org.openqa.selenium.By;
+package ai.iamneo.testing.Testing_Selenium_TestNg;
+import org.testng.annotations.Test;
+import java.net.URL;
+import java.util.List;
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import java.util.List;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
 
-public class FlipkartLinksTest {
-    private WebDriver driver;
-
-    @BeforeTest
-    public void setUp() {
-        // Set up WebDriver (assuming you're using Chrome)
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @Test
-    public void testFlipkartLinks() {
-        // Navigate to Flipkart website
-        driver.get("https://www.flipkart.com");
-
-        // Find all the links on the page
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-
-        // Print the count of links
-        System.out.println("Number of links on Flipkart homepage: " + links.size());
-
-        // Print link text and URLs
-        for (WebElement link : links) {
-            String linkText = link.getText();
-            String linkUrl = link.getAttribute("href");
-            System.out.println("Link Text: " + linkText + " - URL: " + linkUrl);
-        }
-    }
-
-    @AfterTest
-    public void tearDown() {
-        // Close the browser
-        driver.quit();
-    }
+public class AppTest {
+	WebDriver driver = null;
+	String url="https://flipkart.com";
+	ChromeOptions options = new ChromeOptions();
+	@BeforeTest
+	public void beforeTest() throws IOException{
+		System.setProperty("webdriver.chrome.driver", "/home/coder/project/workspace/chromedriver");
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+	}
+	@Test
+	public void testcase_1() throws InterruptedException
+	{
+		driver.manage().window().maximize();
+		driver.get(url);
+		List<WebElement> allLink = driver.findElements(By.tagName("a"));
+		System.out.println("Number of links on a page :: "+allLink.size());
+	}
+	@Test
+	public void testcase_2() throws InterruptedException 
+      {
+		List<WebElement> allLink = driver.findElements(By.tagName("a"));
+		for(WebElement link:allLink){
+			System.out.println("Link Text : "+ link.getText());
+			System.out.println("Link URL  : "+ link.getAttribute("href"));
+		}
+	}
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 }
